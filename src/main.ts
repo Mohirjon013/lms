750e9194617d622c0eb363478eb22431c4e8882a
Mohirjon013 <mohirjon26@gmail.com>
2026-08-08 15:38:57 +0500

    docker-ci/cd

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(9000 , '0.0.0.0');
 
   app.useGlobalPipes(
     new ValidationPipe({
