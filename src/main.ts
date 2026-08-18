8991a711e8c554acc2f46813780c7b0b73623287
Mohirjon013 <mohirjon26@gmail.com>
2026-08-19 03:29:02 +0500

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,15 @@ import { config } from './common/config/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  
   app.setGlobalPrefix('/api/v1');
+  
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://3.71.195.135:3000'], // frontend manzillaringiz
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   
   app.useGlobalPipes(
     new ValidationPipe({
@@ -15,12 +22,12 @@ async function bootstrap() {
       transform: true,
     }),
   );
-
+  
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, documentFactory);
-
+  
   await app.listen(9000 , '0.0.0.0');
-
+  
   
 }
 bootstrap();
